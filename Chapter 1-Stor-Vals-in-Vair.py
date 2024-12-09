50dafa8099121140f5c19e9651471f1f69f9e4fa
Thomas O'Mara <tcpomara@gmail.com>
2024-12-09 02:42:50 +0000

    added-info-in-Stor...

---
+++ Chapter 1-Stor-Vals-in-Vair.py
@@ -0,0 +1,22 @@
+spam = 40
+
+print (spam)
+
+eggs = 2
+
+print (spam + eggs)
+
+spam2 = spam + 3
+
+print (spam2)
+
+text = ("How are you?")
+
+print (text)
+
+text = ("Haha, I changed 'text'")
+
+print (text)
+
+print ("You can change what 'text' means I can change it to print whatever I want which is a usefull thing to remember for the future")
+
